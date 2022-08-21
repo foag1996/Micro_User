f76fa3b40bbaa394ad13c0d09178f65ef877ffe0
foag1996 <foalvarezg@ufpso.edu.co>
2022-08-21 15:45:10 -0500

    metricas

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from typing import Union
 import requests
+from prometheus_fastapi_instrumentator import Instrumentator
 from fastapi import FastAPI, status, Response
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
+Instrumentator().instrument(app).expose(app)
 
 @app.get("/")
 def read_root():
@@ -26,3 +27,4 @@ def infoUser():
     url='https://630264749eb72a839d6ef2ff.mockapi.io/infoUsers'
     response = requests.get(url, {}, timeout=5)
     return response.json()
+
